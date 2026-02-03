ca5bd9bde728c7412dd0e8d0d20432d87cd7d77f
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-02-03 18:33:26 +0100

    Made a first version of files uploads. I'm not proud of it, but it works and it needs a lot of work - Like I would try to see if my error handling is good - I need to add more complex file validation rules preferably using zod - Understand my own code a bit more xD

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,21 +1,47 @@
-import fs from "fs";
+import fs from "fs/promises";
+import path from "path";
+import { v4 } from "uuid";
 
 export async function POST(request: Request) {
   const formData = await request.formData();
-  const files = formData.getAll("files");
+  const files = formData.getAll("files") as File[];
+  let message: string = "";
 
-  try {
-    if (!fs.existsSync("app/uploads")) {
-      console.log("Uploads doesn't extist");
-    }
-    if (!fs.existsSync("public")) {
-      console.log("@Public doesn't work");
-    }
-  } catch (err) {
-    console.log(err);
+  if (files.length === 0) {
+    let message = "No files uploaded.";
+    return new Response(JSON.stringify(message), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 
-  return new Response(JSON.stringify(files), {
+  const uploadedFiles: string[] = [];
+  const upoladDir = path.join(process.cwd(), "public/uploads");
+
+  try {
+    await fs.mkdir(upoladDir, { recursive: true });
+  } catch (error) {
+    console.error("Error creating directory: ", error);
+    return new Response(JSON.stringify("Error creating directory"), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  files.map(async (file) => {
+    try {
+      const filename = `${v4()}-${file.name}`;
+      const buffer = Buffer.from(await file.arrayBuffer());
+      const filePath = path.join(upoladDir, filename);
+      await fs.writeFile(filePath, buffer);
+      uploadedFiles.push(`uploads/${filename}`);
+      message += `File ${file.name} uploaded successfully as ${filename}\n`;
+    } catch (error) {
+      console.error("Error saving file: ", error);
+      message += `Error saving file ${file.name}: ${error}\n`;
+    }
+  });
+
+  return new Response(JSON.stringify(message), {
     status: 201,
     headers: { "Content-Type": "application/json" },
   });
